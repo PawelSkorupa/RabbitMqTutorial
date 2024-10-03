ec7f380211d03713a308107bc89c983d3ca356fe
PawelSkorupa <pawelskorupa01@gmail.com>
2024-10-03 23:25:58 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation("com.rabbitmq:amqp-client:5.17.1")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
